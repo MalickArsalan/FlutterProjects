b149d097e2c79f3d19f533ff03c5efa8cc5712dd
MalickArsalanLFD <malick.arsalan@lovefordata.com>
2023-10-09 15:58:25 +0500

    Completing The First App
    * We have used  `Material App` and `Scaffold`
    * We used Text Formatting
    * We Used `Column` to to show vertically numbers of widgets
    * We used `Image.network()`  to show images from network.
    * We used to `ElevatedButton()` to show `AlertDialog`
    * We used padding to add some space between the widgets.

--- 1.hello_world/lib/main.dart
+++ 1.hello_world/lib/main.dart
@@ -10,11 +10,78 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Hello World Travel',
-        textDirection: TextDirection.ltr,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Helloo World Travel Title",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello World Travel App"),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Builder(builder: (context) {
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        'Hello World Travel',
+                        style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue[800]),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: Text(
+                        'Discover the World',
+                        style: TextStyle(
+                            fontSize: 20, color: Colors.deepPurpleAccent),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Image.network(
+                        'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
+                        height: 350,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: ElevatedButton(
+                        child: const Text('Contact us'),
+                        onPressed: () => contactUs(context),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        }),
       ),
+    );
+  }
+
+  void contactUs(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Contact Us'),
+          content: const Text('Mail us at hello@world.com'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          ],
+        );
+      },
     );
   }
 }
