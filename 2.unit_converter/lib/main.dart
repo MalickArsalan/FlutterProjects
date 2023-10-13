83e0cca00bf9e8f1ea1047f36c349f2d9ae86222
MalickArsalanLFD <malick.arsalan@lovefordata.com>
2023-10-13 12:44:27 +0500

    Completing UI
    * Completing rest of the app UI

--- 2.unit_converter/lib/main.dart
+++ 2.unit_converter/lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _startMeasure = '';
+  String? _startMeasure;
+  String? _convertedMeasure;
 
   late double _numberFrom;
 
   final List<String> _measures = [
-    '',
     'meters',
     'kilometers',
     'grams',
@@ -36,6 +36,16 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    final TextStyle inputStyle = TextStyle(
+      fontSize: 20,
+      color: Colors.blue[900],
+    );
+
+    final TextStyle labelStyle = TextStyle(
+      fontSize: 24,
+      color: Colors.grey[700],
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Measures Converter',
@@ -43,38 +53,112 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Measures Converter'),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              DropdownButton(
-                  value: _startMeasure,
+        body: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const Spacer(),
+                Text(
+                  'Value',
+                  style: labelStyle,
+                ),
+                const Spacer(),
+                TextField(
+                  keyboardType: const TextInputType.numberWithOptions(
+                      signed: false, decimal: true),
+                  style: inputStyle,
+                  decoration: const InputDecoration(
+                    hintText: "Please insert the measure to be converted",
+                  ),
+                  onChanged: (text) {
+                    var rv = double.tryParse(text);
+                    if (rv != null) {
+                      setState(() {
+                        _numberFrom = rv;
+                      });
+                    } else if (text.isEmpty) {
+                      setState(() {
+                        _numberFrom = 0;
+                      });
+                    }
+                  },
+                ),
+                const Spacer(),
+                Text(
+                  'From',
+                  style: labelStyle,
+                ),
+                DropdownButton(
+                  style: inputStyle,
+                  isExpanded: true,
                   items: _measures.map((String value) {
                     return DropdownMenuItem<String>(
-                        value: value, child: Text(value));
+                        value: value,
+                        child: Text(
+                          value,
+                          style: inputStyle,
+                        ));
                   }).toList(),
-                  onChanged: (value) {
+                  onChanged: (String? value) {
                     setState(() {
-                      _startMeasure = value!;
+                      _startMeasure = value;
                     });
-                  }),
-              TextField(
-                onChanged: (text) {
-                  var rv = double.tryParse(text);
-                  if (rv != null && text.isNotEmpty) {
+                  },
+                  value: _startMeasure,
+                ),
+                const Spacer(),
+                Text(
+                  'To',
+                  style: labelStyle,
+                ),
+                const Spacer(),
+                DropdownButton(
+                  style: inputStyle,
+                  isExpanded: true,
+                  items: _measures.map((String value) {
+                    return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                          style: inputStyle,
+                        ));
+                  }).toList(),
+                  onChanged: (String? value) {
                     setState(() {
-                      _numberFrom = rv;
+                      _convertedMeasure = value;
                     });
-                  } else if (text.isEmpty) {
-                    setState(() {
-                      _numberFrom = 0;
-                    });
-                  }
-                },
-              ),
-              Text(
-                (_numberFrom == null) ? '' : _numberFrom.toString(),
-              )
-            ],
+                  },
+                  value: _convertedMeasure,
+                ),
+                const Spacer(
+                  flex: 2,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      elevation: const MaterialStatePropertyAll(4),
+                      backgroundColor:
+                          MaterialStatePropertyAll(Colors.grey[300])),
+                  child: Text(
+                    'Convert',
+                    style: inputStyle,
+                  ),
+                  onPressed: () => true,
+                ),
+                const Spacer(
+                  flex: 2,
+                ),
+                Text(
+                  (_numberFrom == null) ? '' : _numberFrom.toString(),
+                  style: inputStyle,
+                ),
+                const Spacer(
+                  flex: 8,
+                )
+              ],
+            ),
           ),
         ),
       ),
